85606ef259b3493860d9c0162d4085ec32afa98f
lionel kouamou <kouamoumbiakop@gmail.com>
2026-07-18 10:58:02 +0100

    Fix: Remplacement de l'opération simple par l'addition

--- calcul.py
+++ calcul.py
@@ -1,10 +1,10 @@
 # Crée ton script avec ces lignes
-nombre1 = 5
-nombre2 = 12
+nombre1 = 8
+nombre2 = 2
 
 # L'opération de multiplication
-resultat = nombre1 * nombre2
+resultat = nombre1 + nombre2
 
 # Affichage du résultat dans la console
 print(f"L'opération demandée est une multiplication :")
-print(f"{nombre1} x {nombre2} = {resultat}")
+print(f"{nombre1} + {nombre2} = {resultat}")

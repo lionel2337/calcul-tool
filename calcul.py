d3f52f127da0d67a819fa8efcfc4c360968b0230
lionel kouamou <kouamoumbiakop@gmail.com>
2026-07-18 10:46:58 +0100

    Initialisation du projet avec une opération simple

---
+++ calcul.py
@@ -0,0 +1,10 @@
+# Crée ton script avec ces lignes
+nombre1 = 5
+nombre2 = 12
+
+# L'opération de multiplication
+resultat = nombre1 * nombre2
+
+# Affichage du résultat dans la console
+print(f"L'opération demandée est une multiplication :")
+print(f"{nombre1} x {nombre2} = {resultat}")
